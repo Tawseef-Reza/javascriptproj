66ebb4f49e40b8705d096b2a01c868f6881eb930
Tawseef-Reza <65203327+Tawseef-Reza@users.noreply.github.com>
2022-06-18 23:09:42 -0400

    Add files via upload

--- morejs/counter.js
+++ morejs/counter.js
@@ -1,5 +1,9 @@
 let numbersaved = 1;
+const started = document.getElementById("started");
 
+const sleep = function(milliseconds) {
+    return new Promise(function(resolve) { setTimeout(resolve, milliseconds) })
+}
 function time() {
     let isAm = true;
     let d = new Date();
@@ -29,40 +33,77 @@ function time() {
 }
 
 function startit() {
-    if (parseInt(document.getElementById("number").innerHTML) == 0) {
-        document.getElementById("started").innerHTML = `Started ${time()}`;
+    if ((started.innerHTML).includes(`Started`)) { //checks if they already pressed started
+        console.log(started);
+        console.log(started.innerHTML);
+        const previousval = started.innerHTML;
+        
+        document.getElementById("starter").disabled = true;
+        document.getElementById("adder").disabled = true;
+        document.getElementById("saver").disabled = true;
+        
+        started.innerHTML = "Already started.";
+        console.log(started.innerHTML);
+        sleep(300).then(function() {
+            
+            console.log(started.innerHTML);
+            sleep(500).then(function() {
+                console.log("value at first dot is" + started.innerHTML);
+                started.innerHTML = started.innerHTML + ".";
+                sleep(500).then(function() {
+                    console.log("value at first dot is" + started.innerHTML);
+                    started.innerHTML = started.innerHTML + ".";
+                    sleep(500).then(function() {
+                        console.log("value at first dot is" + started.innerHTML);
+                        started.innerHTML = started.innerHTML + ".";
+                        sleep(1500).then(function() {
+                            document.getElementById("starter").disabled = false;
+                            document.getElementById("adder").disabled = false;
+                            document.getElementById("saver").disabled = false;
+                            started.innerHTML = previousval;
+                            console.log("completed");
+                        })
+                        
+                    })
+                })
+            })
+            
+        }
+        )
+        
     }
-    else {
-        document.getElementById("started").innerHTML = `Already counted`;
+    else { //if they didnt already press start, then u can do it
+        document.getElementById("started").innerHTML = `Started ${time()}`;
     }
 }
 function addit() {
     let number = parseInt(document.getElementById("number").innerHTML);
-    if ((document.getElementById("started").innerHTML).includes(`Started`)) {
+    if ((document.getElementById("started").innerHTML).includes(`Started`)) { //checks if it has started
         number++;
         console.log(number);
         document.getElementById("number").innerHTML = number;
     }
-    else {
+    else { //hasn't started, so dont run
         document.getElementById("started").innerHTML = "Haven't Pressed Start Yet!"
     }
     
 }
 function saveit() {
     let information = document.getElementById("saved").innerHTML;
-    if (document.getElementById("number").innerHTML == "0") {
-        information += `<tr> <td> ${numbersaved} </td> <td> ${document.getElementById("number").innerHTML} </td> <td> N/A </td> <td> N/A </td></tr> \n`;
-        document.getElementById("saved").innerHTML = information;
-        document.getElementById("number").innerHTML = 0;
-        numbersaved++;
-        document.getElementById("started").innerHTML = "";
-    }
-    else {
+    if ((document.getElementById("started").innerHTML).includes(`Started`)) {
         let ending = `Ended ${time()}`
         information += `<tr> <td> ${numbersaved} </td> <td> ${document.getElementById("number").innerHTML} </td> <td> ${document.getElementById("started").innerHTML} </td> <td> ${ending} </td></tr> \n`;
         document.getElementById("saved").innerHTML = information;
         document.getElementById("number").innerHTML = 0;
         numbersaved++;  
+        document.getElementById("started").innerHTML = "";
+        
+    }
+    else {
+        information += `<tr> <td> ${numbersaved} </td> <td> N/A </td> <td> N/A </td> <td> N/A </td></tr> \n`;
+        document.getElementById("saved").innerHTML = information;
+        document.getElementById("number").innerHTML = 0;
+        numbersaved++;
         document.getElementById("started").innerHTML = "";
     }
     
